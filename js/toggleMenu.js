4c933389a9ef809b22dadff1ca048dffc6106de5
unknown <gldhperes@gmail.com>
2023-03-14 13:00:16 -0300

    Finalinado o MenuBar

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,7 +1,21 @@
 const menuBtn = $('#menuBtn')
+const openMenu = $("#openMenu")
+const closeMenu = $("#closeMenu")
+const headerWppNAV = $('#headerWppNAV')
+const menuLi = $(".navbarLi")
 
 $(menuBtn).on('click', () => {
     $('#navbar').toggle()
+    $(openMenu).toggle()
+    $(closeMenu).toggle()
+    $(headerWppNAV).toggle()
+})
+
+$("#navbar>ul").on("click", () => {
+    $('#navbar').toggle()
+    $(openMenu).toggle()
+    $(closeMenu).toggle()
+    $(headerWppNAV).toggle()
 })
 
 $( window ).resize( () => {
@@ -14,21 +28,37 @@ function showMenu(){
     const screenWidth = screen.width;
     const windowWidth = window.innerWidth;
 
+    // SE FOR MENOR QUE 900
     if ( screenWidth <= 900 || windowWidth <= 900){
         $('#navbar').css({
             "display":"none"
         })
 
-        $('#menuBtn').css({
+        $(openMenu).css({
             "display":"flex"
+        })
+
+        $(closeMenu).css({
+            "display":"none"
+        })
+
+        $(headerWppNAV).css({
+            "display":"none"
         })
 
     }else{
         $('#navbar').css({
             "display":"flex"
         })
-        $('#menuBtn').css({
+
+        $(closeMenu).css({
             "display":"none"
         })
+
+        $(headerWppNAV).css({
+            "display":"none"
+        })
+
     }
 }
+
